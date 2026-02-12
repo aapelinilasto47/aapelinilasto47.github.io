235419496ff649ba5c76de8897f556c26f55b191
aapeli <aan00001@laurea.fi>
2026-02-12 11:48:51 +0200

    pathfix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aapelinilasto47.github.io/',
+  base: '/src/',
 })
