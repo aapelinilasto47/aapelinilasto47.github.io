07c6aebfb878cf6a6cfee587ecf529aa04a6863b
aapeli <aan00001@laurea.fi>
2026-02-12 21:48:02 +0200

    värjeää

--- src/components/navBlock.jsx
+++ src/components/navBlock.jsx
@@ -1,6 +1,6 @@
 function navBlock({ navItem, onClick}) {
   return (
-    <div onClick={onClick} className="nav-block" style={{ backgroundColor: navItem.color1, borderColor: navItem.color2 }}>
+    <div onClick={onClick} className="nav-block" style={{ color: navItem.color1, borderColor: navItem.color2 }}>
       <h2>{navItem.label}</h2>
     </div>
   );
--- src/data/navitems.js
+++ src/data/navitems.js
@@ -9,8 +9,8 @@ class NavItem {
 }
 
 export const navItems = [
-  new NavItem(1, 'Projects', 'projects', '#c030a8', '#f7a2ff'),
-  new NavItem(2, 'Work', 'work', '#c77c26', '#ffc380'),
-  new NavItem(3, 'Contact', 'contact', '#2bbbb4', '#8afff9'),
-  new NavItem(4, 'Courses', 'courses', '#3128af', '#887bf5')
+  new NavItem(1, 'Projects', 'projects', '#c030a8', '#ea00ff'),
+  new NavItem(2, 'Work', 'work', '#c77c26', '#ff8800'),
+  new NavItem(3, 'Contact', 'contact', '#2bbbb4', '#00fff2'),
+  new NavItem(4, 'Courses', 'courses', '#3128af', '#1900ff')
 ]
