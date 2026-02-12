6efd8b995d504c79c1b107f7a59c258396643cec
aapeli <aan00001@laurea.fi>
2026-02-12 21:55:48 +0200

    navdesign

--- src/data/navitems.js
+++ src/data/navitems.js
@@ -9,8 +9,8 @@ class NavItem {
 }
 
 export const navItems = [
-  new NavItem(1, 'Projects', 'projects', '#c030a8', '#ea00ff'),
-  new NavItem(2, 'Work', 'work', '#c77c26', '#ff8800'),
-  new NavItem(3, 'Contact', 'contact', '#2bbbb4', '#00fff2'),
-  new NavItem(4, 'Courses', 'courses', '#3128af', '#1900ff')
+  new NavItem(1, 'Projects', 'projects', '#db62c7', '#ea00ff'),
+  new NavItem(2, 'Work', 'work', '#e7a75d', '#ff8800'),
+  new NavItem(3, 'Contact', 'contact', '#5ee7e0', '#00fff2'),
+  new NavItem(4, 'Courses', 'courses', '#657af0', '#1900ff')
 ]
