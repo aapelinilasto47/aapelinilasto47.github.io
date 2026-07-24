09092099086e0dfe92bac70121bca6038a0325dc
aapeli <aan00001@laurea.fi>
2026-07-24 12:25:09 +0300

    more projects and work stuff

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,46 +1,51 @@
 class Project {
   constructor(title, technologies, description, link) {
-    this.title = title
-    this.technologies = technologies
-    this.description = description
-    this.link = link
+    this.title = title;
+    this.technologies = technologies;
+    this.description = description;
+    this.link = link;
   }
 }
 
-
 export const projects = [
   new Project(
-    'Django Backend',
-    'Django, REST API, PostgreSQL, Postman, Python',
-    'A backend project using Django framework to create RESTful APIs with PostgreSQL database integration.',
-    'https://github.com/aapelinilasto47/djangoprojekti'
+    "Tournament Prediction Web App",
+    "React.js, Tailwind CSS, Next.js API Routes, Next.js Server Actions, MongoDB Atlas, Google OAuth 2.0, Playwright, Github Actions, Vercel",
+    "A web application that allows users to predict the outcomes of tournaments, with user authentication, data storage in MongoDB Atlas, and automated fixture updates using Playwright.",
+    "https://github.com/aapelinilasto47/mm-veikkaus",
+  ),
+  new Project(
+    "Django Backend",
+    "Django, REST API, PostgreSQL, Postman, Python",
+    "A backend project using Django framework to create RESTful APIs with PostgreSQL database integration.",
+    "https://github.com/aapelinilasto47/djangoprojekti",
   ),
 
   new Project(
-    'Discord Bot',
-    'Python, Discord.py, APIs',
-    'A Discord bot that interacts with users, fetches data from various APIs, and provides useful functionalities within Discord servers.',
-    'https://github.com/aapelinilasto47/dcbotti-2.0'
+    "Discord Bot",
+    "Python, Discord.py, APIs",
+    "A Discord bot that interacts with users, fetches data from various APIs, and provides useful functionalities within Discord servers.",
+    "https://github.com/aapelinilasto47/dcbotti-2.0",
   ),
 
   new Project(
-    'Currency Converter Web App',
-    'HTML, CSS, JavaScript, Vue.js, Rest APIs, Bootstrap',
-    'A web application that allows users to convert currencies using real-time exchange rates fetched from external APIs.',
-    'https://github.com/aapelinilasto47/javascript-project-3'
+    "Currency Converter Web App",
+    "HTML, CSS, JavaScript, Vue.js, Rest APIs, Bootstrap",
+    "A web application that allows users to convert currencies using real-time exchange rates fetched from external APIs.",
+    "https://github.com/aapelinilasto47/javascript-project-3",
   ),
 
   new Project(
-    'Personal Portfolio Website',
-    'HTML, CSS, JavaScript, React.js, Bootstrap',
-    'A personal portfolio website showcasing projects, skills, and experiences, built with React.js for a dynamic user experience.',
-    ''
+    "Personal Portfolio Website",
+    "HTML, CSS, JavaScript, React.js, Bootstrap",
+    "A personal portfolio website showcasing projects, skills, and experiences, built with React.js for a dynamic user experience.",
+    "",
   ),
 
   new Project(
-    'MenuManager Rest API',
-    'Node.js, Express.js, MongoDB, REST API',
-    'A RESTful API for managing restaurant menus, allowing CRUD operations on menu items and categories using Node.js and MongoDB.',
-    'https://github.com/aapelinilasto47/MenuManager'
-  )
-]
+    "MenuManager Rest API",
+    "Node.js, Express.js, MongoDB, REST API",
+    "A RESTful API for managing restaurant menus, allowing CRUD operations on menu items and categories using Node.js and MongoDB.",
+    "https://github.com/aapelinilasto47/MenuManager",
+  ),
+];
